9096998e6a2dfe3af2af26d393e48676157db03b
meijiao <meijiao@gmail.com>
2017-01-05 10:48:49 -0800

    tripleStep with memoization

--- dp_tripleStep.js
+++ dp_tripleStep.js
@@ -1,8 +1,8 @@
 /*   1 2 5  ==> 7
 */
 
-/*Brute force*/
-function bruteCoins (options, total) {
+/*Brute force V1*/
+function bruteSteps (options, total) {
   var count = 0; // init global var
 
   function recurse (options, remainder) {
@@ -24,5 +24,33 @@ function bruteCoins (options, total) {
   return count;
 }
 
-console.log(bruteCoins([1,2,3], 6));
+console.log(bruteSteps([1,2,3], 6));
+
+/*Brute force V2*/
+function bruteStepsB (total) {
+  if (total < 0) {
+    return 0;
+  } else if (total === 0) {
+    return 1;
+  } else {    
+    return bruteStepsB(total - 1) + bruteStepsB(total - 2) + bruteStepsB (total - 3);
+  }
+};
+
+var memo = {};
+
+function findSteps (total, memo) {
+  if (total < 0) {
+    return 0;
+  } else if (total === 0) {
+    return 1;
+  } else if (memo[total] > -1) {
+    return memo[total];
+  } else {
+    memo[total] = findSteps(total - 1, memo) + findSteps(total - 2, memo) + findSteps (total - 3, memo);
+    return memo[total];
+  }
+}
+
+console.log(findSteps(6, memo), 'approach C');
 /*With memoization*/
