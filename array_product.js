536d3b53c479116fae1abc323a86872d328e95e7
meijiao <meijiao@gmail.com>
2017-01-19 16:44:36 -0800

    complete product

--- array_product.js
+++ array_product.js
@@ -1,54 +1,35 @@
-/*
-0.5, 0.5
-
-1/6 1/6 1/6 1/6 1/6 1/6
-
-0.75 0.25
-
-0.1 0.3 0.2 0.4
-
-for each die, there is a list of probs "p"
-where p_i is the prob we land on index i
-
-input: p , array p
-output: callable object that will generate index i with prob p_i
+/* find product of all elements except yourself
+** [1,2,3,4,5]
+** ==> product before you and product after you
+** ==> [1, results[i-1]*a[i-1]]
 */
 
-function roll(arr) {
-  // generate incremental ranges based on prob units
-  const ranges = generateRanges(arr);
-  // look up random against range
-  let num = Math.random();
-  
-  for (var i = 1; i < ranges.length; i++) {
-    if (num < ranges[i]) {
-      return i - 1;
+function findProduct (arr) {
+  var results = [];
+  // find product of everything before you
+  for (var i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      results[i] = 1;
+    } else {
+      results[i] = results[i-1] * arr[i-1];
     }
   }
-  return ranges.length - 1;
-}
-
-function generateRanges (arr) {
-  var myArr = [];
-  var tracker = 0;
-  
-  arr.forEach(function(prob) {
-    myArr.push(tracker);
-    tracker += prob;
-  });
-  
-  return myArr;
-}
-
-var test0 = [0.1, 0.3, 0.2, 0.4];
-
-function rollDie (arr, n, fn) {
-  var results = [0, 0, 0, 0];
-  while (n > 0) {
-    results[fn(arr)]++;
-    n--;
+  // find product of everything after you
+  // ==> [120, 60, 20, 5, 1]
+  var temp = 0;
+  for (var i = arr.length - 1; i>= 0; i--) {
+    console.log(i);
+    if (i === arr.length - 1) {
+      temp = 1;
+    } else {
+      temp = temp * arr[i+1];
+    }
+    // change element at that index
+    results[i] = results[i] * temp;
   }
   return results;
 }
 
-console.log(rollDie(test0, 10000, roll));
+console.log(findProduct([1,2,3,4,5]));
+
+
