0ee5a010cb37e4cc08636057cff018030c30e974
meijiao <meijiao@gmail.com>
2017-01-22 22:41:35 -0800

    Balanced BST

---
+++ problem_overlappingRect.js
@@ -0,0 +1,19 @@
+// helper: check for overlap
+  // if leftX_A + width <= leftX_B, return A
+
+// helper: find left object
+  // take smaller leftX, return index of obj
+// helper: find right object
+  // take larger leftX, return index of obj
+// helper: width
+  // leftX_left + width - leftX_right
+// helper: height
+  // bottomY_right - (bottomY_left + height)
+
+function getOverlap () {
+// Get coordinates of bottom left
+
+// Get width and height  
+}
+
+
---
+++ tree_binaryDepth.js
@@ -0,0 +1,79 @@
+function isBalanced (root) {
+
+ let depths = []; 
+ 
+ // create stack 
+ let stack = [];
+ stack.push([root, 0]);
+
+  // while loop
+ while (stack.length > 0) {
+  // visit node
+  let item = stack.pop();
+  let node = item[0];
+  let depth = item[1];
+
+  // push children
+  if (node.right) {
+   stack.push([node.right, depth + 1]);
+  }
+  if (node.left) {
+    stack.push([node.left, depth + 1]);
+  }
+
+  // if it's a leaf node
+  if (!node.right && !node.left) {
+    if (depths.indexOf(depth) === -1) {
+      depths.push(depth);
+      // check and short-circuit process if unbalanced
+      if (depths.length > 2 || Math.abs(depths[0] - depths[1]) > 1) {
+        return false;
+      }
+    }
+  }
+
+ }
+ console.log(depths);
+ return true;
+}
+
+
+function Node (val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+function BinaryTree (val) {
+  this.root = new Node (val);
+}
+
+BinaryTree.prototype.insert = function(val) {
+  var node = this.root;
+
+  while (node) { 
+    if (val > node.value) {
+      console.log('right', val)
+      if (!node.right) {
+        node.right = new Node (val);
+        break;
+      }
+      node = node.right;
+    } else {
+      console.log('left', val)
+      if (!node.left) {
+        node.left = new Node (val);
+        break;
+      }
+      node = node.left;
+    }
+  }
+}; 
+
+var tree = new BinaryTree(4);
+tree.insert(3);
+tree.insert(2);
+tree.insert(1);
+tree.insert(5);
+
+console.log(isBalanced(tree.root));
