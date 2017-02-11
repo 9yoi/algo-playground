c0bb0267ad882614e5f3a895e398d1dbd36e167e
meijiao <meijiao@gmail.com>
2017-02-10 23:02:43 -0800

    permute

--- string_permutate.js
+++ string_permutate.js
@@ -1,5 +1,5 @@
+// Approach 1: remove 1 char at a time. add all remaining
 var arr = [];
-
 function permutate (subs, curr) {
   if (subs.length < 1) {
     arr.push(curr);
@@ -13,4 +13,22 @@ function permutate (subs, curr) {
   return arr;
 }
 
-console.log(permutate('cats', ''))
+//console.log(permutate('cats', ''))
+
+//Approach 2: slot character in to all possible positions.
+var arr2 = [];
+function permutate2 (string, curr) {
+  if (string.length < 1) {
+    arr2.push(curr);
+    return;
+  }
+  let char = string[0];
+  let remaining = string.slice(1);
+  for (var i = 0; i <= curr.length; i++) {
+    let newSubStr = curr.slice(0, i) + char + curr.slice(i)
+    permutate2(remaining, newSubStr);
+  }
+  return arr2;
+}
+
+console.log(permutate2('cats', ''));
